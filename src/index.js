9d96befff8da82505a8ef2e132bfdf3755b5ed10
nahid na <absiddik718@gmail.com>
2024-03-03 02:36:18 +0600

    removed swagger

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const app = require("./app.js");
 const connectToDatabase = require("./config.js/db.js");
 const { PORT } = require("./secret");
-const swaggerDoc = require("./utils/Swagger.js");
 const { errorMessgae } = require("./utils/response.js");
 
 app.listen(PORT, async () => {
+  
   console.log(`Server Started At Port : ${PORT}`);
+  // Connect To Database
   await connectToDatabase();
-  swaggerDoc(app, PORT);
 
   // Client-Side Route Error.
   app.use((req, res, next) => {
